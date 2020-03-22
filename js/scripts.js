ad0815375950f94b24da32123749c9cc9f8dc507
unknown <ganalez2006@gmail.com>
2020-03-21 20:43:49 -0400

    test

--- js/scripts.js
+++ js/scripts.js
@@ -8,6 +8,7 @@ if (typeof navigator.serviceWorker !== 'undefined') {
 	});
 }
 
+// ---------------- 1. Containers
 var wrap_difficulty = document.getElementById('wrap-difficulty');
 var list_difficulty = document.getElementById('difficulty');
 
@@ -17,9 +18,9 @@ var list_categories = document.getElementById('categories');
 var wrap_trivia = document.getElementById('wrap-trivia');
 var elemnt_question = document.getElementById('question');
 var elemnt_options = document.getElementById('options');
-var elemnt_result = document.getElementById('result');
 var elemnt_description = document.getElementById('description');
 
+// ---------------- 2. function getQuestions
 async function getQuestions() {
 
 	// read our JSON
@@ -39,11 +40,91 @@ async function getQuestions() {
 	return questions;
 }
 
+// ---------------- 3. Execute getQuestions
 getQuestions().then(function(questions) {
 
-	// clear list difficulty
+	function showQuestion(bt_difficulty, bt_category) {
+			
+		// list questions by category
+		var list_questions = questions[bt_difficulty][bt_category];
+
+		// random question
+		var min = 1;
+		var max = list_questions.length;
+		var item = Math.floor(Math.random()*(max));
+		const question = {};
+		const _r = Object.assign(question, list_questions[item]);
+
+		// show question
+		if (typeof question !== 'undefined') {
+
+			// show question
+			elemnt_question.innerHTML = question.t;
+			delete question.t;
+			
+			// set description
+			var description = question.de;
+			elemnt_description.innerHTML = '';
+			delete question.de;
+
+			// set answer
+			var answer = question.r;
+			delete question.r;
+
+			// clear options
+			elemnt_options.innerHTML = '';
+			
+			// show options
+			var options = Object.keys(question);
+			var opt_correct = 0;
+			options.forEach((opt, i=0) => {
+
+				if (opt == answer) {
+					opt_correct = i;
+				}
+				i++;
+
+				var li_opt = document.createElement('li');
+				var t = document.createTextNode(question[opt]);
+				li_opt.appendChild(t);
+
+				// action correct/wrong option
+				li_opt.onclick = () => {
+
+					if (opt == answer) {
+
+						li_opt.classList.add('correct');
+					} else {
+						
+						li_opt.classList.add('wrong');
+						elemnt_options.childNodes[opt_correct].classList.add('correct');
+					}
+					
+					elemnt_description.innerHTML = description;
+
+					// delete question 
+					delete questions[bt_difficulty][bt_category][item];
+					questions[bt_difficulty][bt_category] = questions[bt_difficulty][bt_category].filter(Boolean);
+				};
+
+				// add option to list options
+				elemnt_options.appendChild(li_opt);
+			});
+
+
+			// delete question
+			//delete questions[bt_difficulty][bt_category][item];
+			//questions[bt_difficulty][bt_category] = questions[bt_difficulty][bt_category].filter(Boolean);
+
+			//
+			wrap_trivia.classList.remove('hide');
+		}
+	}
+
+	// ---------------- 4. clear container list difficulty
 	list_difficulty.innerHTML = '';
 
+	// ---------------- 5. show list difficulty
 	var difficulty = Object.keys(questions);
 	difficulty.sort();
 
@@ -58,32 +139,34 @@ getQuestions().then(function(questions) {
 		list_difficulty.appendChild(li_difficulty);
 	});
 
+	// ---------------- 6. listen click event
 	var bt_difficulty = '';
 	var bt_category = '';
-	var list_questions = '';
+	var current_category = '';
 
-	// add click event
 	document.addEventListener('click', function(e) {
 
-		// bt difficulty
+		// ---------------- 7. listen click bt difficulty
 		if(e.target && e.toElement.className == 'bt-difficulty') {
-			//console.debug(e);
+
+			// difficulty value
 			var _difficulty = e.toElement.innerText;
 
-			// set difficulty
+			// show difficulty value
 			var ds = document.getElementsByClassName('difficulty-selected');
 			for (let item of ds) {
 				item.innerHTML = _difficulty;
 			}
 
+			// difficulty value in array questions
 			bt_difficulty = 'd_' + _difficulty;
 			var categories = Object.keys(questions[bt_difficulty]);
 			categories.sort();
 
-			// clear list categories
+			// ---------------- 8. clear container list categories
 			list_categories.innerHTML = '';
 
-			// show categories
+			// ---------------- 9. set list categories
 			categories.forEach(cat => {
 
 				var li_category = document.createElement('li');
@@ -93,91 +176,55 @@ getQuestions().then(function(questions) {
 				li_category.appendChild(t);
 				list_categories.appendChild(li_category);
 			});
+
+			// ---------------- 10. show list categories
 			wrap_categories.classList.remove('hide');
 		}
 
-		// bt Category
+		// ---------------- 11. listen click bt Category
 		if(e.target && e.toElement.className == 'bt-cat') {
-			//console.debug(e.target);
+
+			current_category = e.target;
+
+			// category value
 			bt_category = e.toElement.innerText;
 
-			// set category
-			var ds = document.getElementsByClassName('category-selected');
-			for (let item of ds) {
-				item.innerHTML = bt_category;
-			}
-			
-			// list questions by category
-			var list_questions = questions[bt_difficulty][bt_category];
+			if (questions[bt_difficulty][bt_category].length > 0) {
 
-			// random question
-			var min = 1;
-			var max = list_questions.length;
-			var item = Math.floor(Math.random()*(max));
-			var question = list_questions[item];
+				// show category value
+				var ds = document.getElementsByClassName('category-selected');
+				for (let item of ds) {
+					item.innerHTML = bt_category;
+				}
 
-			// show question
-			if (typeof question !== 'undefined') {
+				// ---------------- 12. show question
+				showQuestion(bt_difficulty, bt_category);
 
-				// show question
-				elemnt_question.innerHTML = question.t;
-				delete question.t;
-				
-				// set description
-				var description = question.de;
-				elemnt_description.innerHTML = '';
-				delete question.de;
-
-				// set answer
-				var answer = question.r;
-				delete question.r;
-
-				// clear options
-				elemnt_options.innerHTML = '';
-				
-				// show options
-				var options = Object.keys(question);
-				options.forEach(opt => {
-
-					var li_opt = document.createElement('li');
-					var t = document.createTextNode(question[opt]);
-					li_opt.appendChild(t);
-
-					// action correct/wrong option
-					li_opt.onclick = () => {
-
-						if (opt == answer) {
-
-							li_opt.classList.add('correct');
-						} else {
-							
-							li_opt.classList.add('wrong');
-						}
-						
-						elemnt_description.innerHTML = description;
-					};
-
-					// add option to list options
-					elemnt_options.appendChild(li_opt);
-				});
-
-				// delete question
-				delete questions[bt_difficulty][bt_category][item];
-				questions[bt_difficulty][bt_category] = questions[bt_difficulty][bt_category].filter(Boolean);
-
-				//
-				wrap_trivia.classList.remove('hide');
-			}
-
-			// remove empty category
-			if (questions[bt_difficulty][bt_category].length == 0) {
+			} else {
 				console.debug('No hay mas preguntas');
 				e.target.parentNode.removeChild(e.target);
 			}
 		}
 
+		// hide section
 		if(e.target && e.toElement.className == 'bt-close') {
-			e.target.parentNode.classList.add('hide');
+			e.target.parentNode.parentNode.classList.add('hide');
+		}
+
+		// next question
+		if(e.target && e.toElement.className == 'bt-next') {
+
+			if (questions[bt_difficulty][bt_category].length > 0) {
+			
+				showQuestion(bt_difficulty, bt_category);
+
+			} else {
+
+				// remove empty category
+				console.debug('No hay mas preguntas');
+				current_category.parentNode.removeChild(current_category);
+				wrap_trivia.classList.add('hide');
+			}
 		}
 	});
 });
